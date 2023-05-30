adab3a5b10a6ecabd83d33f4b51442febef327e2
Raul Pires <raulpires224@gmail.com>
2023-05-29 23:46:22 -0300

    Modificando model de registro

--- Dominio/Dto/Request/RegisterModel.cs
+++ Dominio/Dto/Request/RegisterModel.cs
@@ -5,5 +5,5 @@ public class RegisterModel
     public string Email { get; set; }
     public string Name { get; set; }
     public string Password { get; set; }
-    public string PasswordValidate { get; set; }
+    public string PasswordValidation { get; set; }
 }
